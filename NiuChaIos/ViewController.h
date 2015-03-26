861f7ef07bf152dd94ff5fb7630cc983bcdedd78
SharkCome <dtp0254@sharkcome.local>
2015-03-26 13:27:02 +0800

    Initial Commit

---
+++ NiuChaIos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NiuChaIos
+//
+//  Created by SharkCome on 15/3/26.
+//  Copyright (c) 2015年 gujianjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NiuChaIos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NiuChaIos
+//
+//  Created by SharkCome on 15/3/26.
+//  Copyright (c) 2015年 gujianjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
